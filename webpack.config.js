ec13f62385e0be218d14b6ef9d57964efa418e68
jsl.luallen@gmail.com <jsl.luallen@gmail.com>
2021-11-20 10:14:10 -0500

    removed depricated site artifects

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    publicPath: "/librarybuild/",
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
